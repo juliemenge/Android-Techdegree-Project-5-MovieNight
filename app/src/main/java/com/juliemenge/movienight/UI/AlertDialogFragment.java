a86868ccda0bf5446497e434cc471d6534850200
Julie Menge <juliemenge@gmail.com>
2017-06-25 21:36:39 -0400

    undoing comment change

--- app/src/main/java/com/juliemenge/movienight/UI/AlertDialogFragment.java
+++ app/src/main/java/com/juliemenge/movienight/UI/AlertDialogFragment.java
@@ -18,6 +18,5 @@ public class AlertDialogFragment extends DialogFragment {
 
         AlertDialog dialog = builder.create(); //actually create the alert dialog
         return dialog;
-        // non-change
     }
 }

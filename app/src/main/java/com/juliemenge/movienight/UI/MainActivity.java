f6f8628b4c39d9853878b7a8af058b6a87afb152
Julie Menge <juliemenge@gmail.com>
2017-07-09 12:41:59 -0400

    sort by options

--- app/src/main/java/com/juliemenge/movienight/UI/MainActivity.java
+++ app/src/main/java/com/juliemenge/movienight/UI/MainActivity.java
@@ -11,11 +11,13 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 import android.widget.Toast;
 
+import com.juliemenge.movienight.Data.Genre;
 import com.juliemenge.movienight.Data.Movie;
 import com.juliemenge.movienight.R;
 
@@ -24,6 +26,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -45,13 +49,22 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private Movie[] mMovie; //creating a variable to store movie results - call it on onResponse when response is successful
 
     //use butterknife to declare all UI variables
+    @BindView(R.id.tvCheckBox) CheckBox mTVCheckBox;
     @BindView(R.id.votesEntry) EditText mVotesEntry;
     @BindView(R.id.ratingEntry) EditText mRatingEntry;
     @BindView(R.id.startDateEntry) EditText mStartDateEntry;
     @BindView(R.id.endDateEntry) EditText mEndDateEntry;
     @BindView(R.id.submitButton) Button mSubmitButton;
     @BindView(R.id.genreSpinner) Spinner mGenreSpinner;
-    public String[] genreNames = {"", "Action", "Adventure", "Animation"};
+    public String[] genreNames = {"", "Action", "Adventure", "Animation", "Comedy", "Crime", "Documentary", "Drama",
+                                    "Family", "Fantasy", "History", "Horror", "Music", "Mystery", "Romance", "Science Fiction",
+                                    "TV Movie", "Thriller", "War", "Western"};
+    @BindView(R.id.sortSpinner) Spinner mSortSpinner;
+    public String[] sortOptions = {"", "Popularity", "Release Date", "Revenue", "Average Vote", "Number of Votes"};
+
+
+    //private Genre[] mGenre;
+    //private List<Genre> genreList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,11 +73,20 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         ButterKnife.bind(this); //make butterknife do its thing
 
+        //populateGenres(); //make request to get list of genres
+
         //create the spinner for genres
         ArrayAdapter genreAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, genreNames);
         genreAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mGenreSpinner.setAdapter(genreAdapter);
         mGenreSpinner.setOnItemSelectedListener(this);
+
+        //create the spinner for sort options
+        final ArrayAdapter sortAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, sortOptions);
+        sortAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSortSpinner.setAdapter(sortAdapter);
+        mSortSpinner.setOnItemSelectedListener(this);
+
 
 
         //when the button is clicked, do the api request and return the results in a new activity
@@ -86,15 +108,68 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                         break;
                     case "Animation":  genre = "16";
                         break;
-
-                    default: genre= "Invalid genre";
+                    case "Comedy": genre="35";
                         break;
+                    case "Crime": genre="80";
+                        break;
+                    case "Documentary": genre="99";
+                        break;
+                    case "Drama": genre="18";
+                        break;
+                    case "Family": genre="10751";
+                        break;
+                    case "Fantasy": genre="14";
+                        break;
+                    case "History": genre="36";
+                        break;
+                    case "Horror": genre="27";
+                        break;
+                    case "Music": genre="10402";
+                        break;
+                    case "Mystery": genre="9648";
+                        break;
+                    case "Romance": genre="10749";
+                        break;
+                    case "Science Fiction": genre="878";
+                        break;
+                    case "TV Movie": genre="10770";
+                        break;
+                    case "Thriller": genre="53";
+                        break;
+                    case "War": genre="10752";
+                        break;
+                    case "Western": genre="37";
+                        break;
+                    default: genre= ""; //do not include genre in request if none specified
+                        break;
+                }
+
+                //set sort variable based on what was selected
+                String sortBy;
+                switch (mSortSpinner.getSelectedItem().toString()) {
+                    case "Popularity": sortBy = "popularity.desc";
+                        break;
+                    case "Release Date": sortBy = "release_date.asc";
+                        break;
+                    case "Revenue": sortBy = "revenue.asc";
+                        break;
+                    case "Average Vote": sortBy = "vote_average.asc";
+                        break;
+                    case "Number of Votes": sortBy = "vote_count.asc";
+                        break;
+                    default: sortBy = ""; //do not include sort in request if none specified
+                        break;
+                }
+
+                //check if tv show button is checked
+                if(mTVCheckBox.isChecked()) {
+                    Toast.makeText(MainActivity.this, "Box is checked!", Toast.LENGTH_LONG).show();
                 }
 
 
                 //build the api url
                 String movieUrl = "https://api.themoviedb.org/3/discover/movie?api_key=" + apiKey +
-                        "&language=en-US&sort_by=popularity.desc&include_adult=false" +
+                        "&language=en-US&sort_by=" + sortBy + "100&include_adult=false" +
                         "&include_video=false&page=1&vote_average.gte=" + ratingAverage +
                         "&vote_count.gte=" + votesCount + "&primary_release_date.gte=" + startDate +
                         "&primary_release_date.lte=" + endDate + "&with_genres=" + genre; //add + genrevariable
@@ -218,6 +293,93 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         AlertDialogFragment dialog = new AlertDialogFragment();
         dialog.show(getFragmentManager(), "error_dialog");
     }
+
+    /*
+    //get current available genres from tmdb api
+    public void populateGenres() {
+        String genreUrl = "https://api.themoviedb.org/3/genre/movie/list?api_key=313b7986e3fac321ab33d6d3546ac8ab&language=en-US";
+
+        if(isNetworkAvailable()) {
+
+            OkHttpClient client = new OkHttpClient();
+
+            Request request = new Request.Builder()
+                    .url(genreUrl)
+                    .build();
+
+            Call call = client.newCall(request);
+
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    //what to do when there is no response
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                        }
+                    });
+                    alertUserAboutError(); //if no response, let the user know
+                }
+
+                @Override
+                //what to do when you receive a response back
+                public void onResponse(Call call, Response response) throws IOException {
+                    try {
+                        final String jsonData = response.body().string(); //string to store all the json data
+                        if (response.isSuccessful()) { //if you receive a response and it is successful
+                            mGenre = getGenreResults(jsonData); //pass the json data into the method that creates the movies model
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    updateDisplay(); //need to check if i need this for refreshing?
+
+                                }
+                            });
+                        } else { //if you receive a response and it is NOT successful
+                            alertUserAboutError(); //let user know there was an error
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+                    } catch (JSONException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+                    }
+
+                }
+            });
+        } else { //if the network is NOT available, let the user know
+            Toast.makeText(MainActivity.this, "Network unavailable!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private Genre[] getGenreResults(String jsonData) throws JSONException {
+        genreList = new ArrayList<>();
+
+        JSONObject allData = new JSONObject(jsonData); //creating a json object to store everything possible from the api request
+
+        //create a new json object of just the array of genre results
+        JSONArray results = allData.getJSONArray("genres");
+
+        Genre[] genres = new Genre[results.length()]; //create an array of genres of the same length of however long the array of results is from the api request
+
+        //loop through each item in the results array and assign it to an element of the movies array
+        for(int i=0; i<results.length(); i++) {
+            JSONObject jsonGenre = results.getJSONObject(i); //create a new json object at proper element of results
+            Genre genre = new Genre(); //create a new genre object
+
+            //set the values of a genre
+            genre.setId(jsonGenre.getInt("id"));
+            genre.setName(jsonGenre.getString("name"));
+
+            genreList.add(genre);
+
+
+            genres[i] = genre; //set the element of the genres array to the object we just populated
+        }
+
+        return genres; //return the array of genres
+    }
+    */
 
     //required methods for spinner
     @Override
